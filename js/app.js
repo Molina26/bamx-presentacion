9c715d990721d1d6061cc87c65b9b07cfc00d26f
Manuel A. Molina <maldana261200@gmail.com>
2022-12-15 15:05:12 -0600

    add service worker

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-// if (navigator.serviceWorker) {
-//   navigator.serviceWorker.register('/sw.js');
-// }
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js');
+}
 
 const btnCamera = document.getElementById("btnCamera");
 const btnMakePhoto = document.getElementById("btnMakePhoto");
